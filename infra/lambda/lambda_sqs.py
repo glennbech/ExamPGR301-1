4d138c97ca058b6e0e26eb966ecddab12f2b4f57
Siggyy1 <92021288+Siggyy1@users.noreply.github.com>
2024-11-20 12:42:49 +0000

    fixed lambda_sqs file for throttlingExeption

--- infra/lambda/lambda_sqs.py
+++ infra/lambda/lambda_sqs.py
@@ -1,25 +1,22 @@
-import base64
+import time
+import random
 import boto3
 import json
-import random
 import os
-
-# Frank; This is probalby be closer to what we need in production. Let's get funding first!
+from botocore.exceptions import ClientError
 
 bedrock_client = boto3.client("bedrock-runtime", region_name="us-east-1")
 s3_client = boto3.client("s3")
 
 MODEL_ID = "amazon.titan-image-generator-v1"
-BUCKET_NAME =  os.environ["BUCKET_NAME"]
+BUCKET_NAME = os.environ["BUCKET_NAME"]
 
 def lambda_handler(event, context):
-    # Loop through all SQS records in the event
     for record in event["Records"]:
-        # Extract the SQS message body
         prompt = record["body"]
         seed = random.randint(0, 2147483647)
         s3_image_path = f"images/titan_{seed}.png"
-        # Prepare the request for image generation
+
         native_request = {
             "taskType": "TEXT_IMAGE",
             "textToImageParams": {"text": prompt},
@@ -33,20 +30,25 @@ def lambda_handler(event, context):
             },
         }
 
-        # Invoke the model
-        response = bedrock_client.invoke_model(
-            modelId=MODEL_ID,
-            body=json.dumps(native_request)
-        )
+        retries = 0
+        max_retries = 5
+        while retries < max_retries:
+            try:
+                response = bedrock_client.invoke_model(
+                    modelId=MODEL_ID,
+                    body=json.dumps(native_request)
+                )
+                model_response = json.loads(response["body"].read())
+                base64_image_data = model_response["images"][0]
+                image_data = base64.b64decode(base64_image_data)
 
-        model_response = json.loads(response["body"].read())
-        base64_image_data = model_response["images"][0]
-        image_data = base64.b64decode(base64_image_data)
+                s3_client.put_object(Bucket=BUCKET_NAME, Key=s3_image_path, Body=image_data)
+                break
+            except ClientError as e:
+                if e.response['Error']['Code'] == 'ThrottlingException':
+                    retries += 1
+                    time.sleep(2 ** retries)  # Eksponentiell venting
+                else:
+                    raise e
 
-        # Upload the image to S3
-        s3_client.put_object(Bucket=BUCKET_NAME, Key=s3_image_path, Body=image_data)
-
-    return {
-        "statusCode": 200,
-        "body": json.dumps("")
-    }
+    return {"statusCode": 200, "body": json.dumps("")}
