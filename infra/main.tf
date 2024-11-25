03586c063680042546242dc13338d06ab2e4f244
Siggyy1 <92021288+Siggyy1@users.noreply.github.com>
2024-11-25 10:27:26 +0000

    batch_size fix

--- infra/main.tf
+++ infra/main.tf
@@ -92,7 +92,7 @@ resource "aws_lambda_function" "image_processor" {
 resource "aws_lambda_event_source_mapping" "sqs_lambda_mapping" {
   event_source_arn = aws_sqs_queue.image_processing_queue.arn
   function_name    = aws_lambda_function.image_processor.arn
-  batch_size       = 10
+  batch_size       = 1
   enabled          = true
 }
 
